98df84990a03fda2e37c4d41bec9725e26407779
Tony Waffo <waffotony@gmail.com>
2024-09-26 09:49:47 -0400

    experience section updated with containers

--- src/Experience.js
+++ src/Experience.js
@@ -1,9 +1,50 @@
 import "./Experience.css";
 
+import aboutPic from "./aboutPic.webp";
+
 const Experience=()=>{
+    const experienceList=[
+        {
+            title:"Software engeneer intern",
+            company:"Logitech",
+            period:" April 2024 - now",
+            description:"Description for the job",
+            picture:aboutPic,
+        },
+        {
+            title:"Software engeneer intern",
+            company:"Logitech",
+            period:" April 2024 - now",
+            description:"Description for the job",
+            picture:aboutPic,
+        },
+        {
+            title:"Software engeneer intern",
+            company:"Logitech",
+            period:" April 2024 - now",
+            description:"Description for the job",
+            picture:aboutPic,
+        },
+    ]
     return (
         <>
-        <section className="experience">Experience</section>
+        <section className="experience">
+            <h1>Experience</h1>
+            <div className="experience-list">
+                {experienceList.map((experience,index)=>(
+                    <div className= {`${"experience-container"} ${index%2==1 ? '':'reverse'}`}>
+                        <div className="experience-img">
+                            <img src={experience.picture} height="100%" width="100%"></img>
+                        </div>
+
+                        <div className="experience-content">
+                            <h3>{experience.title}|{experience.company}<br/>{experience.period}</h3>
+                            <p>{experience.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
         </>
     );
 }
